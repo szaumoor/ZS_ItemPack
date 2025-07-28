9cbbba0124fb7e3c3a72dc9c83a8206b34bc3afb
szaumoor <email>
2025-07-28 19:35:07 +0200

    fixed install errors

--- ZS_ItemPack/dlg/quest/zsipbern.d
+++ ZS_ItemPack/dlg/quest/zsipbern.d
@@ -1,9 +1,9 @@
 EXTEND_BOTTOM BERNARD 31
-+ ~Global("TalkedToSethle","GLOBAL",1) Global("ZSIlmaterQuest","GLOBAL",0)~ + @0 /* Is there anywhere one can listen to harpsong here? */ DO ~SetGlobal("ZSIlmaterQuest","GLOBAL",1)~ + ZS.Harpsong
++ ~Global("TalkedToSethle","GLOBAL",1) Global("ZSIlmaterQuest","GLOBAL",0)~ + @30030 /* Is there anywhere one can listen to harpsong here? */ DO ~SetGlobal("ZSIlmaterQuest","GLOBAL",1)~ + ZS.Harpsong
 END
 
 CHAIN BERNARD ZS.Harpsong
-@1 /* I wouldn't know, <PRO_SIRMAAM>. But friends might seek out a priest, Akachi, in the temple above. They say he hears more than prayers. */
-== JAHEIRAJ IF ~InParty("Jaheira") InMyArea("Jaheira") !StateCheck("Jaheira",CD_STATE_NOTVALID)~ THEN @2 /* A delicate touch that. Shall I stomp about like an ogre while you're at it? */
+@30031 /* I wouldn't know, <PRO_SIRMAAM>. But friends might seek out a priest, Akachi, in the temple above. They say he hears more than prayers. */
+== JAHEIRAJ IF ~InParty("Jaheira") InMyArea("Jaheira") !StateCheck("Jaheira",CD_STATE_NOTVALID)~ THEN @30032 /* A delicate touch that. Shall I stomp about like an ogre while you're at it? */
 END
-IF ~~ DO ~AddJournalEntry(@3001, QUEST)~ EXIT
+IF ~~ DO ~AddJournalEntry(@30001, QUEST)~ EXIT
--- ZS_ItemPack/dlg/quest/zsipmum.d
+++ ZS_ItemPack/dlg/quest/zsipmum.d
@@ -1,19 +1,19 @@
 BEGIN ZSIPMUM
 
 CHAIN IF ~NumTimesTalkedTo(0)~ THEN ZSIPMUM ZS.UndeadAttack
-@0 /* The living rarely tread here after dark. You are no kin to the others - those who feast on mortal blood - nor do you carry the plague. Why have you come? Do you want to join us in undeath? */ 
+@30040 /* The living rarely tread here after dark. You are no kin to the others - those who feast on mortal blood - nor do you carry the plague. Why have you come? Do you want to join us in undeath? */
 END
-++ @1 /* I'm searching for the source of a sickness that's stricken the slums. Are you behind it? */ EXTERN ZSIPMUM ZS.UndeadAttack1
-++ @2 /* My reasons are of no concern to you. But you have no right to linger here. */ EXTERN ZSIPMUM ZS.UndeadAttack1
-++ @3 /* Undeath is no gift. I’ve come to end your corruption. */ EXTERN ZSIPMUM ZS.UndeadAttack1
+++ @30041 /* I'm searching for the source of a sickness that's stricken the slums. Are you behind it? */ EXTERN ZSIPMUM ZS.UndeadAttack1
+++ @30042 /* My reasons are of no concern to you. But you have no right to linger here. */ EXTERN ZSIPMUM ZS.UndeadAttack1
+++ @30043 /* Undeath is no gift. I’ve come to end your corruption. */ EXTERN ZSIPMUM ZS.UndeadAttack1
 
 CHAIN ZSIPMUM ZS.UndeadAttack1
-@4 /* The sickness is a gift we offer freely. It takes away the pain of living. No more hunger. No more suffering. Only stillness. Only mercy. */
+@30044 /* The sickness is a gift we offer freely. It takes away the pain of living. No more hunger. No more suffering. Only stillness. Only mercy. */
 END
-++ @5 /* You call this mercy? Speak to the dying in Ilmater's temple and see what comfort they've found. */ EXTERN ZSIPMUM ZS.UndeadAttack2
-++ @6 /* I've heard enough. Go back to your graves, or be forced back into them. */ EXTERN ZSIPMUM ZS.UndeadAttack2
+++ @30045 /* You call this mercy? Speak to the dying in Ilmater's temple and see what comfort they've found. */ EXTERN ZSIPMUM ZS.UndeadAttack2
+++ @30046 /* I've heard enough. Go back to your graves, or be forced back into them. */ EXTERN ZSIPMUM ZS.UndeadAttack2
 
 CHAIN ZSIPMUM ZS.UndeadAttack2
-@7 /* We will not be turned from our cause. Death claims all in time. */ 
-= @8 /* ...Some sooner than others. Strike, my kin, let the dust claim this intruder. */
-DO ~Enemy() AddJournalEntry(@3004, QUEST) SetGlobal("ZSIlmaterQuest","GLOBAL",3)~ EXIT
+@30047 /* We will not be turned from our cause. Death claims all in time. */
+= @30048 /* ...Some sooner than others. Strike, my kin, let the dust claim this intruder. */
+DO ~Enemy() AddJournalEntry(@30004, QUEST) SetGlobal("ZSIlmaterQuest","GLOBAL",3)~ EXIT
